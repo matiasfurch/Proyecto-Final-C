e05eadc63528466364fe0b3cd15bc027e340cc81
Gabriel Scalise <97moviforplan08@gmail.com>
2018-10-27 08:56:44 -0300

    Initial Commit

---
+++ Proyecto Final C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Proyecto Final C
+//
+//  Created by Gabriel S on 27/10/2018.
+//  Copyright © 2018 Gabriel S. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
